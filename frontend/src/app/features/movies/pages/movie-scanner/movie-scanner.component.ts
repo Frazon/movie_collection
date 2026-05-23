646cdaff9accbf4dfdef654023e5815948d700f1
Airton Frazon <contato@frazon.com.br>
2026-05-23 15:58:17 -0300

    Improve barcode scanner UX and mobile responsiveness

--- frontend/src/app/features/movies/pages/movie-scanner/movie-scanner.component.ts
+++ frontend/src/app/features/movies/pages/movie-scanner/movie-scanner.component.ts
@@ -17,19 +17,32 @@ export class MovieScannerComponent {
 
   private movieService = inject(MovieService);
 
+  isProcessing: boolean = false;
+
   scannedBarcode = '';
 
   hasMovie: boolean | null = null;
 
   onCodeResult(barcode: string) {
 
+    if (this.isProcessing) {
+      return;
+    }
+
+    this.isProcessing = true;
+
     this.scannedBarcode = barcode;
 
     this.movieService.existsMovie(barcode)
       .subscribe({
         next: (response) => {
-
           this.hasMovie = response;
+          setTimeout(() => {
+            this.isProcessing = false;
+          }, 3000);
+        },
+        error: () => {
+          this.isProcessing = false;
         }
       });
   }
